a74c0b61391a110701241e6b21bedb51779c647c
Victor Taiwo <51122045+Smallz97@users.noreply.github.com>
2025-04-16 16:16:31 +0100

    removed the cart wrapper component

--- src/app/utilities/ui/components/cart-wrapper/CartWrapper.tsx
+++
@@ -1,5 +0,0 @@
-import CartButton from "@/app/utilities/ui/components/buttons/CartButton"
-
-export default function CartWrapper() {
-    return <CartButton initialCartData={{ items: [], totalItems: 0 }} />
-}
